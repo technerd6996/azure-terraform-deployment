b7c826a721a23fe3162c45a6ba5a7732a93ede05
VedaVidyadaran S <svedavidyadaran@gmail.com>
2026-06-13 23:51:39 +0530

    Workflow file modified

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -92,17 +92,17 @@ resource "azurerm_linux_virtual_machine" "main" {
 }
 
 resource "azurerm_network_security_rule" "http" {
-  name                        = "AllowHTTP"
-  priority                    = 110
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
+  name      = "AllowHTTP"
+  priority  = 110
+  direction = "Inbound"
+  access    = "Allow"
+  protocol  = "Tcp"
 
-  source_port_range           = "*"
-  destination_port_range      = "80"
+  source_port_range      = "*"
+  destination_port_range = "80"
 
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
+  source_address_prefix      = "*"
+  destination_address_prefix = "*"
 
   resource_group_name         = azurerm_resource_group.main.name
   network_security_group_name = azurerm_network_security_group.main.name
